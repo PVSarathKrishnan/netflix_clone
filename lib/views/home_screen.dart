45ae9810dce71744e9956840adb3f958333f719b
Sarath Krishnan <pvsarathkrishnan@gmail.com>
2024-02-09 23:47:17 +0530

    image adjustments

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,9 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:netflix_clone/style/text_style.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -14,7 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.black, // Adjust app bar color
         elevation: 0,
         title: Image.asset(
           "lib/assets/logo.png",
@@ -24,30 +23,61 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 15),
-            child: Text("Trending Movies", style: Headings),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black87, Colors.black54], // Add background gradient
           ),
-          SizedBox(
-            width: double.infinity,
-            child: CarouselSlider.builder(
-              itemCount: 10,
-              options: CarouselOptions(
-                height: 300
+        ),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18.0, vertical: 15),
+                child: Text(
+                  "Trending Movies",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-              itemBuilder: (context, itemIndex, pageViewIndex) {
-                return Container(
-                  height: 300,
-                  width: 200,
-                  color: Colors.blue,
-                );
-              },
-            ),
-          ) 
-        ]),
+              SizedBox(
+                height: 300,
+                child: CarouselSlider.builder(
+                  itemCount: 10,
+                  options: CarouselOptions(
+                    height: 300,
+                    viewportFraction: 0.6, // Adjust visible item size
+                    enableInfiniteScroll: true, // Enable infinite scroll
+                    autoPlay: true, // Enable auto-play
+                    autoPlayInterval:
+                        Duration(seconds: 3), // Set auto-play interval
+                  ),
+                  itemBuilder: (context, itemIndex, pageViewIndex) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              "https://via.placeholder.com/200x300"), // Use actual movie poster image
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      margin: EdgeInsets.symmetric(horizontal: 5),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
